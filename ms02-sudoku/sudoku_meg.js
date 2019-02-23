76818fde2d1fee54035162bae5c4cc0d41830f58
mcontoor <mcontoor@gmail.com>
2019-02-23 11:58:01 +0530

    trying to solve puzzle1

--- ms02-sudoku/sudoku_meg.js
+++ ms02-sudoku/sudoku_meg.js
@@ -1,55 +1,135 @@
-// var p1 = new Array(9)())1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--
-// var p2 = --5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3
-// var p3 = 29-5----77-----4----4738-129-2--3-648---5--7-5---672--3-9--4--5----8-7---87--51-9
-// var p4 = -8--2-----4-5--32--2-3-9-466---9---4---64-5-1134-5-7--36---4--24-723-6-----7--45-
-// var p5 = 6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--
-// var p6 = ---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----
-// var p7 = -3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--
-// var p8 = -96-4---11---6---45-481-39---795--43-3--8----4-5-23-18-1-63--59-59-7-83---359---7
-// var p9 = ----754----------8-8-19----3----1-6--------34----6817-2-4---6-39------2-53-2-----
-// var p10 = 3---------5-7-3--8----28-7-7------43-----------39-41-54--3--8--1---4----968---2--
-// var p11 = 3-26-9--55--73----------9-----94----------1-9----57-6---85----6--------3-19-82-4-
-// var p12 = -2-5----48-5--------48-9-2------5-73-9-----6-25-9------3-6-18--------4-71----4-9-
-// var p13 = --7--8------2---6-65--79----7----3-5-83---67-2-1----8----71--38-2---5------4--2--
-// var p14 = ----------2-65-------18--4--9----6-4-3---57-------------------73------9----------
-// var p15 = ---------------------------------------------------------------------------------
+var p1 = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+// var p2 = "--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3";
+// var p3 = "29-5----77-----4----4738-129-2--3-648---5--7-5---672--3-9--4--5----8-7---87--51-9";
+// var p4 = "-8--2-----4-5--32--2-3-9-466---9---4---64-5-1134-5-7--36---4--24-723-6-----7--45-";
+// var p5 = "6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--";
+// var p6 = "---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----";
+// var p7 = "-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--";
+// var p8 = "-96-4---11---6---45-481-39---795--43-3--8----4-5-23-18-1-63--59-59-7-83---359---7";
+// var p9 = "----754----------8-8-19----3----1-6--------34----6817-2-4---6-39------2-53-2-----";
+// var p10 = "3---------5-7-3--8----28-7-7------43-----------39-41-54--3--8--1---4----968---2--";
+// var p11 = "3-26-9--55--73----------9-----94----------1-9----57-6---85----6--------3-19-82-4-";
+// var p12 = "-2-5----48-5--------48-9-2------5-73-9-----6-25-9------3-6-18--------4-71----4-9-";
+// var p13 = "--7--8------2---6-65--79----7----3-5-83---67-2-1----8----71--38-2---5------4--2--";
+// var p14 = "----------2-65-------18--4--9----6-4-3---57-------------------73------9----------";
+// var p15 = "---------------------------------------------------------------------------------";
 
-// var Chai = require('chai'),
-//     expect = Chai.expect,
-//     solver = require('./sudoku_solver');
 
-// describe('Sudoku Solver', function() {
-//   // all tests should be inserted here
-// });
 
-var board = '090000006\n' + 
-            '000960485\n' +
-            '000581000\n' +
-            '004000000\n' +
-            '517200900\n' +
-            '602000370\n' +
-            '100804020\n' +
-            '706000810\n' +
-            '300090000';
-var parsedBoard;
+var sudokustring = p1.split("").map(Number);
 
-describe('#parseBoard()', function() {
-    it('should parse a sudoku board into a 2D array', function() {
-      parsedBoard = parseBoard(board);
-      var expectedBoard = [
-        [0,9,0,0,0,0,0,0,6],
-        [0,0,0,9,6,0,4,8,5],
-        [0,0,0,5,8,1,0,0,0],
-        [0,0,4,0,0,0,0,0,0],
-        [5,1,7,2,0,0,9,0,0],
-        [6,0,2,0,0,0,3,7,0],
-        [1,0,0,8,0,4,0,2,0],
-        [7,0,6,0,0,0,8,1,0],
-        [3,0,0,0,9,0,0,0,0]
-      ];
-  
-      expect(parsedBoard.length).to.equal(9);
-      expect(parsedBoard[0].length).to.equal(9);
-      expect(parsedBoard).to.eql(expectedBoard);
-    });
-  });
+function sudokuboard() {
+  var array1 = [];
+for (var i = 0; i < 9; i++) {
+  var arr = [];
+  for (var j = 0; j < 9; j++) {
+    arr.push(sudokustring[(i * 9) + j]);
+  }
+  array1.push(arr);
+}
+return array1;
+};
+
+var board = sudokuboard();
+console.log(board);
+
+
+function saveEmptyPositions() {
+  var emptyPositions = [];
+  for (var i=0; i<board.length; i++) {
+    for (var j=0; j<board[i].length; i++) {
+      if (board[i][j] === NaN) {
+        emptyPositions.push(i,j);
+      }
+    }
+  }
+  return emptyPositions;
+};
+
+console.log(saveEmptyPositions());
+
+// var checkRow = function(board,row,value) {
+//   for (var i=0; i<board[row].length; i++) {
+//     if (board[row][i] === value) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// checkRow(board,0,1);
+
+// var checkCol = function(board, column, value) {
+//   for (var j=0; j< board.length; j++) {
+//     if (board[j][column] === value) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// var checkBlock = function(board, column, row, value) {
+//   var colCorner = 0.
+//       rowCorner = 0,
+//       squareSize = 3;
+//   while (column >= colCorner + squareSize) {
+//     colCorner += squareSize;
+//   }
+//   while (row >= rowCorner + squareSize) {
+//     rowCorner += squareSize;
+//   }
+//   for (var i = rowCorner; i<rowCorner + squareSize; i++) {
+//     for (var j = colCorner; j<colCorner + squareSize; j++) {
+//       if (board[i][j] === value) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+
+// var checkValue = function(board, column, row, value) {
+//   if(this.checkRow(board,row,value) &&
+//     this.checkCol(board,column,value) &&
+//     this.checkBlock(board,column,row,value)) {
+//       return true;
+//     } else {
+//       return false;
+//     }   
+// };
+
+// var solvePuzzle = function(board, emptyPositions) {
+//   var limit = 9,
+//     i, row, column, found;
+//   for(var i=0; i<emptyPositions.length;) {
+//     row = emptyPositions[i][0];
+//     column = emptyPositions[i][1];
+//     value = board[row][column] + 1;
+//     found = false;
+//   while (!found && value <= limit) {
+//     if (this.checkValue(board, column, row, value)) {
+//       found = true;
+//       board[row][column] = value;
+//       i++;
+//     }
+//     else {
+//       value++;
+//     }
+//   }
+//   if (!found) {
+//     board[row][column] = 0;
+//     i--;
+//     }
+//   }
+//   board.array.forEach(function(row) {
+//     console.log(row.join());
+//   });
+//     return board;
+//   };
+
+// var solveSudoku = function(board) {
+//   var parsedBoard = this.parsedBoard(board);
+//   var emptyPositions = this.saveEmptyPositions(parsedBoard);
+
+//   return this.solvePuzzle(parsedBoard, emptyPositions);
+// };
